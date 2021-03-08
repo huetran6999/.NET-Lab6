46061393579d683ec64a1cd65b81dc47f9bd30b4
huetran6999 <74998265+huetran6999@users.noreply.github.com>
2021-03-08 15:44:42 +0700

    Bai 2 - Lab 6

---
+++ ThongtinCanBo/ThongtinCanBo/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿
+namespace ThongtinCanBo
+{
+    partial class frmCanbo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTTCanbo = new System.Windows.Forms.Label();
+            this.lblMSCB = new System.Windows.Forms.Label();
+            this.lblHoTenCB = new System.Windows.Forms.Label();
+            this.lblChucvu = new System.Windows.Forms.Label();
+            this.lblGiogiang = new System.Windows.Forms.Label();
+            this.lblDongia = new System.Windows.Forms.Label();
+            this.txtMSCB = new System.Windows.Forms.TextBox();
+            this.txtHoTenCB = new System.Windows.Forms.TextBox();
+            this.txtGiogiang = new System.Windows.Forms.TextBox();
+            this.txtDongia = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.cboChucvu = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // lblTTCanbo
+            // 
+            this.lblTTCanbo.AutoSize = true;
+            this.lblTTCanbo.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTTCanbo.Location = new System.Drawing.Point(147, 29);
+            this.lblTTCanbo.Name = "lblTTCanbo";
+            this.lblTTCanbo.Size = new System.Drawing.Size(315, 36);
+            this.lblTTCanbo.TabIndex = 0;
+            this.lblTTCanbo.Text = "THÔNG TIN CÁN BỘ";
+            // 
+            // lblMSCB
+            // 
+            this.lblMSCB.AutoSize = true;
+            this.lblMSCB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMSCB.Location = new System.Drawing.Point(46, 107);
+            this.lblMSCB.Name = "lblMSCB";
+            this.lblMSCB.Size = new System.Drawing.Size(71, 25);
+            this.lblMSCB.TabIndex = 1;
+            this.lblMSCB.Text = "MSCB";
+            // 
+            // lblHoTenCB
+            // 
+            this.lblHoTenCB.AutoSize = true;
+            this.lblHoTenCB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHoTenCB.Location = new System.Drawing.Point(46, 166);
+            this.lblHoTenCB.Name = "lblHoTenCB";
+            this.lblHoTenCB.Size = new System.Drawing.Size(128, 25);
+            this.lblHoTenCB.TabIndex = 2;
+            this.lblHoTenCB.Text = "Họ và tên CB";
+            // 
+            // lblChucvu
+            // 
+            this.lblChucvu.AutoSize = true;
+            this.lblChucvu.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblChucvu.Location = new System.Drawing.Point(46, 216);
+            this.lblChucvu.Name = "lblChucvu";
+            this.lblChucvu.Size = new System.Drawing.Size(85, 25);
+            this.lblChucvu.TabIndex = 3;
+            this.lblChucvu.Text = "Chức vụ";
+            // 
+            // lblGiogiang
+            // 
+            this.lblGiogiang.AutoSize = true;
+            this.lblGiogiang.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGiogiang.Location = new System.Drawing.Point(46, 271);
+            this.lblGiogiang.Name = "lblGiogiang";
+            this.lblGiogiang.Size = new System.Drawing.Size(121, 25);
+            this.lblGiogiang.TabIndex = 4;
+            this.lblGiogiang.Text = "Số giờ giảng";
+            // 
+            // lblDongia
+            // 
+            this.lblDongia.AutoSize = true;
+            this.lblDongia.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDongia.Location = new System.Drawing.Point(46, 330);
+            this.lblDongia.Name = "lblDongia";
+            this.lblDongia.Size = new System.Drawing.Size(79, 25);
+            this.lblDongia.TabIndex = 5;
+            this.lblDongia.Text = "Đơn giá";
+            // 
+            // txtMSCB
+            // 
+            this.txtMSCB.Location = new System.Drawing.Point(222, 111);
+            this.txtMSCB.Name = "txtMSCB";
+            this.txtMSCB.Size = new System.Drawing.Size(335, 22);
+            this.txtMSCB.TabIndex = 6;
+            // 
+            // txtHoTenCB
+            // 
+            this.txtHoTenCB.Location = new System.Drawing.Point(222, 170);
+            this.txtHoTenCB.Name = "txtHoTenCB";
+            this.txtHoTenCB.Size = new System.Drawing.Size(335, 22);
+            this.txtHoTenCB.TabIndex = 7;
+            // 
+            // txtGiogiang
+            // 
+            this.txtGiogiang.Location = new System.Drawing.Point(222, 275);
+            this.txtGiogiang.Name = "txtGiogiang";
+            this.txtGiogiang.Size = new System.Drawing.Size(335, 22);
+            this.txtGiogiang.TabIndex = 8;
+            // 
+            // txtDongia
+            // 
+            this.txtDongia.Location = new System.Drawing.Point(222, 334);
+            this.txtDongia.Name = "txtDongia";
+            this.txtDongia.Size = new System.Drawing.Size(335, 22);
+            this.txtDongia.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(97, 416);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 40);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Thêm";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(254, 416);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 40);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Lưu";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(413, 416);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(109, 40);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Đóng";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // cboChucvu
+            // 
+            this.cboChucvu.FormattingEnabled = true;
+            this.cboChucvu.Items.AddRange(new object[] {
+            "Giảng viên"});
+            this.cboChucvu.Location = new System.Drawing.Point(226, 214);
+            this.cboChucvu.Name = "cboChucvu";
+            this.cboChucvu.Size = new System.Drawing.Size(330, 24);
+            this.cboChucvu.TabIndex = 13;
+            // 
+            // frmCanbo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(626, 502);
+            this.Controls.Add(this.cboChucvu);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtDongia);
+            this.Controls.Add(this.txtGiogiang);
+            this.Controls.Add(this.txtHoTenCB);
+            this.Controls.Add(this.txtMSCB);
+            this.Controls.Add(this.lblDongia);
+            this.Controls.Add(this.lblGiogiang);
+            this.Controls.Add(this.lblChucvu);
+            this.Controls.Add(this.lblHoTenCB);
+            this.Controls.Add(this.lblMSCB);
+            this.Controls.Add(this.lblTTCanbo);
+            this.Name = "frmCanbo";
+            this.Text = "Can bo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTTCanbo;
+        private System.Windows.Forms.Label lblMSCB;
+        private System.Windows.Forms.Label lblHoTenCB;
+        private System.Windows.Forms.Label lblChucvu;
+        private System.Windows.Forms.Label lblGiogiang;
+        private System.Windows.Forms.Label lblDongia;
+        private System.Windows.Forms.TextBox txtMSCB;
+        private System.Windows.Forms.TextBox txtHoTenCB;
+        private System.Windows.Forms.TextBox txtGiogiang;
+        private System.Windows.Forms.TextBox txtDongia;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox cboChucvu;
+    }
+}
+
