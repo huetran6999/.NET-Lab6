1463795cdc772fc5d926a7ba297440b6b2ad9562
Nguyen Nhat Son <nguyennhatson1810@gmail.com>
2021-03-08 15:34:20 +0700

    YO

---
+++ prjDVD/prjDVD/classes/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prjDVD.classes
+{
+    class clsDatabase
+    {
+        public static SqlConnection con;
+
+        public static bool OpenConnection()
+        {
+            try
+            {
+                con = new SqlConnection("Data Source=localhost,3306;Database=DVDLibrary;User ID=mylogin;Password=mylogin");
+                con.Open();
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }   
+    }
+}
